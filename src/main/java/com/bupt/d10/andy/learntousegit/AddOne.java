d2b9bb48a9a96cdda9c78bbddf9b209589794937
Andy <lkasdolka222@gmail.com>
2015-01-04 18:23:18 +0800

    third change

--- src/main/java/com/bupt/d10/andy/learntousegit/AddOne.java
+++ src/main/java/com/bupt/d10/andy/learntousegit/AddOne.java
@@ -1,7 +1,15 @@
 package com.bupt.d10.andy.learntousegit;
 
+import android.content.SyncStatusObserver;
+import android.util.Log;
+import android.widget.Toast;
+
 /**
  * Created by andy on 2015/1/4.
  */
 public class AddOne {
+    public void  showNumber(){
+        Log.i("test","test");
+    }
+    private int numberTwo = 5;
 }
--- src/main/java/com/bupt/d10/andy/learntousegit/AddTwo.java
+++ src/main/java/com/bupt/d10/andy/learntousegit/AddTwo.java
@@ -4,4 +4,5 @@ package com.bupt.d10.andy.learntousegit;
  * Created by andy on 2015/1/4.
  */
 public class AddTwo {
+    private int number = 0;
 }
